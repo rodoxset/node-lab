b927b91a446b68afbfd6bd3a59ad6c7f15d10b36
rodoxset <52052722+rodoxset@users.noreply.github.com>
2022-01-07 12:04:53 -0300

    Alteração da pagina

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Teste lab DevOps!');
+  res.send('Nova versão lab DevOps!');
 });
 
 app.get('*', (req, res) => {
