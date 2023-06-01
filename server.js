67300952363f32c0523ca84773385732ff011215
rodoxset <52052722+rodoxset@users.noreply.github.com>
2023-06-01 12:57:56 -0300

    Update server.js - nova versão

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Nova versão lab DevOps 3!');
+  res.send('Teste de Nova versão !');
 });
 
 app.get('*', (req, res) => {
